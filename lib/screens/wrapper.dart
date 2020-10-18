24431201825c05cae013b3c6b26ee4c2b2ebbf30
Youcef Messelem <youcefmesselem@Youcefs-MacBook-Pro.local>
2020-10-17 21:17:14 -0700

    Added basic app structure

---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,16 @@
+import 'package:brew_crew_project/screens/authenticate/authenticate.dart';
+import 'package:brew_crew_project/screens/home/home.dart';
+import 'package:flutter/material.dart';
+
+
+
+ class Wrapper extends StatelessWidget {
+   @override
+   Widget build(BuildContext context) {
+
+     //return either a home or authenticate widget
+     return Authenticate(
+
+     );
+   }
+ }
