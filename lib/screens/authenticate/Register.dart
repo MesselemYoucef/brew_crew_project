80315e5dbf92d90e5f84e6d118099609fb20961e
Youcef Messelem <messelemy@gmail.com>
2020-10-24 17:06:25 +0400

    added the colors for register and sign in

--- lib/screens/authenticate/Register.dart
+++ lib/screens/authenticate/Register.dart
@@ -2,7 +2,6 @@ import 'package:brew_crew_project/screens/authenticate/authenticate.dart';
 import 'package:brew_crew_project/services/AuthService.dart';
 import 'package:flutter/material.dart';
 
-
 class Register extends StatefulWidget {
   Function toggleView;
   Register({this.toggleView});
@@ -27,70 +26,64 @@ class _RegisterState extends State<Register> {
         title: Text("Sign up to Brew Crew"),
         actions: [
           FlatButton.icon(
-            onPressed: (){
-              widget.toggleView();
-            }, 
-            icon: Icon(Icons.person), 
-            label: Text("Sign In")
-            )
+              onPressed: () {
+                widget.toggleView();
+              },
+              icon: Icon(
+                Icons.person,
+                color: Colors.white,
+              ),
+              label: Text(
+                "Sign In",
+                style: TextStyle(color: Colors.white),
+              ))
         ],
       ),
       body: Container(
-        padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: <Widget>[
-              SizedBox(height: 20.0),
-              TextFormField(
-                validator: (val) => val.isEmpty ? 'Enter an email' : null,
-                onChanged: (val){
-                  setState(() {
-                    email = val;
-                  });
-                }
-              ),
-              SizedBox(height: 20.0),
-              TextFormField(
-                validator: (val) => val.length < 6 ? 'Enter password 6+ char long' : null,
-                onChanged:(val){
-                  setState(() {
-                    password = val;
-                  });
-                },
-                obscureText: true,
-              ),
-              SizedBox(height: 20.0),
-              RaisedButton(
-                color: Colors.pink[400],
-                child: Text(
-                  "Register",
-                  style: TextStyle(
-                    color: Colors.white
+          padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+          child: Form(
+              key: _formKey,
+              child: Column(children: <Widget>[
+                SizedBox(height: 20.0),
+                TextFormField(
+                    validator: (val) => val.isEmpty ? 'Enter an email' : null,
+                    onChanged: (val) {
+                      setState(() {
+                        email = val;
+                      });
+                    }),
+                SizedBox(height: 20.0),
+                TextFormField(
+                  validator: (val) =>
+                      val.length < 6 ? 'Enter password 6+ char long' : null,
+                  onChanged: (val) {
+                    setState(() {
+                      password = val;
+                    });
+                  },
+                  obscureText: true,
+                ),
+                SizedBox(height: 20.0),
+                RaisedButton(
+                  color: Colors.pink[400],
+                  child: Text(
+                    "Register",
+                    style: TextStyle(color: Colors.white),
                   ),
-                  ),
-                  onPressed: () async{
-                    if(_formKey.currentState.validate()){
-                      dynamic result = await _auth.registerWithEmailAndPassword(email, password);
-                      if(result == null){
+                  onPressed: () async {
+                    if (_formKey.currentState.validate()) {
+                      dynamic result = await _auth.registerWithEmailAndPassword(
+                          email, password);
+                      if (result == null) {
                         setState(() => error = 'Please supply a valid email');
                       }
                       print(error);
                     }
                   },
-              ),
-              SizedBox(height: 12.0),
-              Text(
-                error,
-                style: TextStyle(
-                  color: Colors.red,
-                  fontSize: 14.0
-                )
-                )
-            ]
-          )
-          )
-      ),
+                ),
+                SizedBox(height: 12.0),
+                Text(error, style: TextStyle(color: Colors.red, fontSize: 14.0))
+              ]))),
     );
   }
 }
--- lib/services/AuthService.dart
+++ lib/services/AuthService.dart
@@ -11,8 +11,10 @@ class AuthService {
   }
 
   //auth change user stream
-  Stream<UserModel> get user{
-    return _auth.authStateChanges().map((User user) => _userFromFirebaseUser(user));
+  Stream<UserModel> get user {
+    return _auth
+        .authStateChanges()
+        .map((User user) => _userFromFirebaseUser(user));
   }
 
   //Signin anonymously
@@ -29,25 +31,26 @@ class AuthService {
 
   //Sign in with email and password
 
-    Future signInWithEmailAndPassword(String email, String password) async{
-      try{
-        UserCredential result = await _auth.signInWithEmailAndPassword(email: email, password: password);
-        User user = result.user;
-        return _userFromFirebaseUser(user);
-      }catch(e){
-        print(e.toString());
-        return null;
-      }
-    }
-
-
-  //register with email and password
-    Future registerWithEmailAndPassword(String email, String password) async{
-    try{
-      UserCredential result = await _auth.createUserWithEmailAndPassword(email: email, password: password); 
+  Future signInWithEmailAndPassword(String email, String password) async {
+    try {
+      UserCredential result = await _auth.signInWithEmailAndPassword(
+          email: email, password: password);
       User user = result.user;
       return _userFromFirebaseUser(user);
-    }catch(e){
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+
+  //register with email and password
+  Future registerWithEmailAndPassword(String email, String password) async {
+    try {
+      UserCredential result = await _auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+      User user = result.user;
+      return _userFromFirebaseUser(user);
+    } catch (e) {
       print(e.toString());
       return null;
     }
@@ -55,7 +58,7 @@ class AuthService {
 
   //Sign out
 
-  Future signOut() async{
+  Future signOut() async {
     try {
       return await _auth.signOut();
     } catch (e) {
